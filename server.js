50c164c630a352fc7a61316838f730bea1d645e3
sacpv04 <sacpv04@gmail.com>
2023-03-23 16:21:51 +0700

    update call api

--- server.js
+++ server.js
@@ -3,31 +3,25 @@ const path = require('path');
 const cluster = require('cluster');
 const http = require('http');
 const app = express();
-const Fs = require('fs')
-const request = require('request');
-// sendFile will go here
+const https = require('https');
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 app.get('/build', function(req, res) {
-  console.log(JSON.stringify(req.query.ip));
-  if (cluster.isWorker) {
-    console.log(cluster.worker);
-  }
-
+  let url = `http://${req.query}:8081/build`;
+  http.get(url);
   res.sendFile(path.join(__dirname, '/build.html'));
 });
 app.get('/test', function(req, res) {
-  if (cluster.isWorker) {
-    console.log(cluster.worker);
-  }
+  let url = `http://${req.query}:8081/test`;
+  http.get(url);
   res.sendFile(path.join(__dirname, '/test.html'));
 });
 app.get('/analysis', function(req, res) {
-  if (cluster.isWorker) {
-    console.log(cluster.worker);
-  }
+  let url = `http://${req.query}:8081/analysys`;
+  http.get(url);
   res.sendFile(path.join(__dirname, '/analysis.html'));
 });
-app.listen(3001);
+app.listen(8080);
 
